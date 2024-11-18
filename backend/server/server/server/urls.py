487345400b8b4760ce2e8a62c2f254273a00e663
kttmnwhore <128729139+kttmnwhore@users.noreply.github.com>
2024-11-18 17:03:00 +0300

    update services

--- backend/server/server/server/urls.py
+++ backend/server/server/server/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', include('new_admin.urls')),
     path('', include('pages.urls'))
 ]
