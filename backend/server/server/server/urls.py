02a451badf0630082709d301edbdf0b1b2742ebb
kttmnwhore <128729139+kttmnwhore@users.noreply.github.com>
2024-12-18 13:34:57 +0300

    .

--- backend/server/server/server/urls.py
+++ backend/server/server/server/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('media', include('media.urls')),
-    path('actual', include('actual.urls')),
-    path('appeals', include('appeals.urls')),
-    path('category', include('category.urls')),
-    path('admin', include('users.urls')),
+    path('media/', include('media.urls')),
+    path('actual/', include('actual.urls')),
+    path('appeals/', include('appeals.urls')),
+    path('category/', include('category.urls')),
+    path('admin/', include('users.urls')),
 ]
 
